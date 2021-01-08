c78b3bc00f4e41b8525604ce74d38b4b4bc5c8d1
Manolingam N <manolingam1996@gmail.com>
2021-01-09 00:10:42 +0530

    possible NaN bug fix

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -68,6 +68,18 @@ class Form extends Component {
     });
 
     total_payment_input.addEventListener('change', (event) => {
+      if (
+        event.target.value === '' ||
+        event.target.value === '0' ||
+        Number(event.target.value) < 0
+      ) {
+        alert('Total payment cannot be zero, negative or empty!');
+        total_payment_input.value = '1';
+        this.setState({
+          milestones: 1
+        });
+        return;
+      }
       total_payment = event.target.value;
       const payments = milestone_payments_calculation(
         total_payment,
@@ -88,6 +100,18 @@ class Form extends Component {
     });
 
     milestones_input.addEventListener('change', (event) => {
+      if (
+        event.target.value === '0' ||
+        event.target.value === '' ||
+        Number(event.target.value) < 0
+      ) {
+        alert('Milestone cannot be zero, negative or empty!');
+        milestones_input.value = '1';
+        this.setState({
+          total_payment: 1
+        });
+        return;
+      }
       milestones = event.target.value;
       const payments = milestone_payments_calculation(
         this.state.total_payment,
